f1d9317381709f3f595ed655c82699ed6a225de1
Ignacio Rodriguez <inz@inzvaio.thaiprogrammer.asia>
2016-05-09 18:58:49 +0700

    more debugging info

--- grabtest.cpp
+++ grabtest.cpp
@@ -20,12 +20,12 @@ bool selectOrange(int width, int height, unsigned char *yuv) {
 		int g = (int) (Y - 0.34414 * (Cb - 0x80) - 0.71414 * (Cr - 0x80));
 		int b = (int) (Y + 1.77200 * (Cb - 0x80));
 		
-		if ((r > 180) && (r > g) && ((r-g) > 40) && (g > b)) {
+		if (((r-g) > 20) && (g > b) && ((g-b) > 20)) {
 			orangeCount++;
 		}
  	}
 	 auto factor = (orangeCount * 100.0 / (width * height));
-	 std::cout << factor << std::endl;
+	 std::cout << "orange: " << orangeCount << ", " << factor << "%" << std::endl;
 	 return factor > 1.0; // more than 1% orange
 }
 
