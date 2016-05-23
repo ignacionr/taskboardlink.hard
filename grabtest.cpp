a1345a324dccacca344863764266c63e75a71fdd
Ignacio Rodriguez <inz@inzvaio.thaiprogrammer.asia>
2016-05-23 13:49:05 +0700

    added web-controlled movement

--- grabtest.cpp
+++ grabtest.cpp
@@ -14,14 +14,38 @@ static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
 
 Cam cam;
+std::string ok_response("OK");
 
 void ev_handler(struct mg_connection *nc, int ev, void *p) {
 	auto hm = (struct http_message *) p;
 	if (ev == MG_EV_HTTP_REQUEST) {
 		if (mg_vcmp(&hm->uri, "/current.jpg") == 0) {
 			cam.grabber().Capture("./web/current.jpg");
+			mg_serve_http(nc, hm, s_http_server_opts);
 		}
-		mg_serve_http(nc, hm, s_http_server_opts);
+		else if (mg_vcmp(&hm->uri, "/up") == 0) {
+			cam.pantilt().up();
+			mg_send_head(nc, 200, ok_response.size(), NULL);
+			mg_send(nc, ok_response.c_str(), ok_response.size());
+		}
+		else if (mg_vcmp(&hm->uri, "/down") == 0) {
+			cam.pantilt().down();
+			mg_send_head(nc, 200, ok_response.size(), NULL);
+			mg_send(nc, ok_response.c_str(), ok_response.size());
+		}
+		else if (mg_vcmp(&hm->uri, "/left") == 0) {
+			cam.pantilt().left();
+			mg_send_head(nc, 200, ok_response.size(), NULL);
+			mg_send(nc, ok_response.c_str(), ok_response.size());
+		}
+		else if (mg_vcmp(&hm->uri, "/right") == 0) {
+			cam.pantilt().right();
+			mg_send_head(nc, 200, ok_response.size(), NULL);
+			mg_send(nc, ok_response.c_str(), ok_response.size());
+		}
+		else {
+			mg_serve_http(nc, hm, s_http_server_opts);
+		}
 	}
 }
 
@@ -35,8 +59,6 @@ int main(int argc, char *argv[]) {
 	// Set up HTTP server parameters
 	mg_set_protocol_http_websocket(nc);
 	s_http_server_opts.document_root = "./web";  // Serve current directory
-	// s_http_server_opts.dav_document_root = ".";  // Allow access via WebDav
-	// s_http_server_opts.enable_directory_listing = "yes";
 
 	printf("Starting web server on port %s\n", s_http_port);
 	for (;;) {
