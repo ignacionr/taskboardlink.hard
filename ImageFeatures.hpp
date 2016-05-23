00ce3f25fa2f7738d5c03b2bf774dcde79aa7e67
Ignacio Rodriguez <inz@inzvaio.thaiprogrammer.asia>
2016-05-23 19:12:41 +0700

    Adjusted for better catching big pieces

--- ImageFeatures.hpp
+++ ImageFeatures.hpp
@@ -1,15 +1,16 @@
 class ImageFeatures : public std::vector<std::pair<int,int>> {
 	int	_threshold;
 	#define MAX_FEATURES	10000
+	#define	GRAIN			5
 public:
 	ImageFeatures(int width, int height, unsigned char *data) {
 		bool tested = false;
 		for(_threshold = 10; size() >= MAX_FEATURES || !tested; _threshold++) {
 			clear();
 			int up = *data;
-			for (int y = 2; y < height-1 && size() < MAX_FEATURES; y++) {
+			for (int y = 2; y < height-1 && size() < MAX_FEATURES; y+= GRAIN) {
 				int left = data[(1 + y * width) * 3];
-				for (int x = 2; x < width-1 && size() < MAX_FEATURES; x++) {
+				for (int x = 2; x < width-1 && size() < MAX_FEATURES; x+= GRAIN) {
 					// given a certain point, use it as a feature if its brightness differs sensibly from right or up
 					int point = data[(x + y * width) * 3];
 					if (abs(left-point) > _threshold) {
